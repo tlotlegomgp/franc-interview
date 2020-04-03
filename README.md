4f9544301c3f5d9701e8d864d54aca8d505ab492
Talitha <talkruger9@gmail.com>
2020-04-03 15:36:45 +0200

    completed challenge

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 ### A Franc coding challenge.
 
 # The Challange - A social media time line
+
 A timeline consists of all your tweets plus all the tweets of people you follow ordered chronologically.
 
 # Notes
-* We don't expect you to spend all day on this. We are just looking for a functional proto-type.
-* Feel free to contact us to clarify an details. We are here to help you.
+
+- We don't expect you to spend all day on this. We are just looking for a functional proto-type.
+- Feel free to contact us to clarify an details. We are here to help you.
 
 # Instructions on running the application
 
@@ -20,3 +22,9 @@ A timeline consists of all your tweets plus all the tweets of people you follow 
 1. Please reach out to us for anything
 2. This is a flask application - here is some documentation: https://flask.palletsprojects.com/en/1.1.x/
 3. Python https://www.python.org/
+
+#
+
+# Challenge Completed
+
+Please install the dependencies with `pip install -r requirements.txt` and run `python app.py` to start the application
--- app.py
+++ app.py
@@ -1,23 +1,52 @@
 from flask import Flask, render_template, jsonify, Response, request
+import json
+from datetime import datetime
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index_view():
     username = request.args.get('username')
-    return render_template('index.html', username = username)
+    return render_template('index.html', username=username)
+
 
 @app.route('/users')
 def users_view():
     with open('./users.json', 'r') as f:
-        users = f.read()
-    return Response(users, mimetype="application/json")
+        users = json.loads(f.read())
+    return render_template('users.html', users=users)
+
 
 @app.route('/posts')
 def posts_view():
     with open('./posts.json', 'r') as f:
-        posts = f.read()
-    return Response(posts, mimetype="application/json")
+        # read the json from posts.json
+        posts = json.loads(f.read())
+    return render_template('posts.html', posts=posts)
 
+ # date sorting function from first to last according to date
+
+
+def sort(data, name):
+    return sorted(data[name], key=lambda x: datetime.strptime(x['time'], '%Y-%m-%dT%H:%M:%SZ'), reverse=False)
+
+# implementing user timelines
+@app.route('/timeline/<name>')
+# pass in the name of the user we are
+def timeline_view(name):
+    with open('./posts.json', 'r') as f:
+        data = json.loads(f.read())
+        # grab the user data from the posts.json file
+    return render_template('timelines.html', data={
+        # implement the sort function on the user's post's
+        'posts': sort(data, name),
+        'user': name,
+        # grab the length of the posts made
+        'postsAmount': len(data[name])
+    })
+
+
+# enabled debug mode for refreshing
 if __name__ == '__main__':
-    app.run(host='127.0.0.1')
+    app.run(debug=True)
